5d4a2af5524d8dc7ecffbfd6d6ac71f0703a45d0
Mike Tria <metamike345@yahoo.com>
2013-03-17 11:23:44 -0700

    Standard Rails 3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrecisionWeb::Application.config.secret_token = 'e6452154daa6f95c4f11528956176e51e69a8296d7ebbaacbdcc4d784f6edc395afa435299cf9cf640c8b8a4ad23adee6a8b32d7a32b59714da103bc93dfe9bc'
